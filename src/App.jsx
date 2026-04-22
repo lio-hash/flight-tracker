768924c47f182d29527b99c8d917da74d90f5f92
Leonardo Zeng <leozengy@gmail.com>
2026-04-21 22:22:07 -0400

    Added airport markers for KSUS”

--- src/App.jsx
+++ src/App.jsx
@@ -196,6 +196,7 @@ const airports = [
   {icao: "KMKY", name: "Marco Island Airport", lat: 26.000584, lon: -81.675186 },
   {icao: "KMCN", name: "Middle Georgia Regional Airport", lat: 32.701049, lon: -83.650046 },
   {icao: "KTCL", name: "Tuscaloosa Regional Airport", lat: 33.221597, lon: -87.610313 },
+  {icao: "KSUS", name: "Spirit of St Louis Airport", lat: 38.663837, lon: -90.646475 },
 ]
 export default function App() {
   const [filter, setFilter] = useState("all");
